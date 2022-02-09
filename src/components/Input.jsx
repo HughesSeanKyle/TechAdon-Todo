34c026f8be0fecb13570ac6bcf9e57a3451d17e5
HughesSeanKyle <khughessean@yahoo.com>
2022-02-09 11:41:36 +0200

    Implemented input helper function to update todo state

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,7 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Input = () => {
-	return <div>Input</div>;
+	// State - (Pass this state to ShowTodos as props for mapping)
+	// Set as empty string by default
+	const [todo, setTodo] = useState('');
+	// Set as empty array by default (State to hold all Todos - Will eventually be localStorage)
+	const [todos, setTodos] = useState([]);
+
+	// Helper function to update todo state
+	const handleChange = (e) => {
+		setTodo(e.target.value);
+	};
+
+	// Set a helper function to handle submit
+	// - - OnSubmit be sure to revert todo back to empty string
+
+	// Apply useEffect to update the component once todos state is updated
+
+	return (
+		<div className="container-fluid pt-5">
+			<div className="mb-5">
+				<h1>Todo List</h1>
+			</div>
+			<div className="input-group mb-3 px-5">
+				<input
+					type="text"
+					className="form-control"
+					placeholder="Add Todo"
+					aria-label="Add Todo"
+					aria-describedby="basic-addon1"
+					value={todo}
+					onChange={handleChange}
+				/>
+				<button className="btn btn-primary" title="Add Todo">
+					+
+				</button>
+			</div>
+		</div>
+	);
 };
 
 export default Input;
