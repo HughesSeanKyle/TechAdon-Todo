cad73f0b7a8b538d807d97b6100fcc88e00f974f
HughesSeanKyle <khughessean@yahoo.com>
2022-02-09 10:43:29 +0200

    Created needed components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import './App.css';
+import Input from './components/Input';
+import ShowTodo from './components/ShowTodo';
 
 function App() {
-	return <div className="App">HelloWorld</div>;
+	return (
+		<div className="App">
+			<Input />
+			<ShowTodo />
+		</div>
+	);
 }
 
 export default App;
